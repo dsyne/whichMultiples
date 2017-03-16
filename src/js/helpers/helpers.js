13406b2fe574adebfbc11d73ec51b0fe3533221e
dsyne <tomdsyne@gmail.com>
2017-03-16 08:17:06 +0000

    Remove/add class helpers and tests

--- src/js/helpers/helpers.js
+++ src/js/helpers/helpers.js
@@ -6,14 +6,23 @@ const createNode = nodeString => {
     return tempNode.content.firstChild
 }
 
-const toggleClass = (node, cls) => 
-  hasClass(node, cls) ? node.classList.remove(cls) : node.classList.add(cls)
-
+const toggleClass = (node, cls) => {
+  return node.classList.contains(cls) ? 
+    node.classList.remove(cls) : node.classList.add(cls)
+}
 const hasClass = (node, cls) => 
-  node.classList.contains(cls) || null
+  node.classList.contains(cls) || false
+
+const removeClass = (node, cls) => 
+    node.classList.contains(cls) ? node.classList.remove(cls): null
+
+const addClass = (node, cls) => 
+    node.classList.contains(cls) ? null: node.classList.add(cls)
 
 export {
     createNode,
     toggleClass,
     hasClass,
+    removeClass,
+    addClass,
 }
--- src/js/helpers/helpers.spec.js
+++ src/js/helpers/helpers.spec.js
@@ -1,6 +1,6 @@
 import 'jsdom-global/register'
 import chai  from 'chai'
-import { createNode, toggleClass, hasClass } from './helpers'
+import { createNode, toggleClass, hasClass, removeClass, addClass } from './helpers'
 const expect = chai.expect
 
 describe('Helpers module', () => {
@@ -13,36 +13,57 @@ describe('Helpers module', () => {
   })
 
   describe('toggleClass()', () => {
-      it('Should add a class to the node provided', () => {
-        const node = document.createElement('i')
-        const testClass = 'test'
-        toggleClass(node, testClass)
+    it('Should add a class to the node provided', () => {
+      const node = document.createElement('i')
+      const testClass = 'test'
+      toggleClass(node, testClass)
 
-        expect(node.classList[0]).to.equal(testClass)
-      })    
-      it('Should remove a class from the node provided', () => {
-        const node = document.createElement('i')
-        const testClass = 'test'
-        node.classList.add(testClass)
-        toggleClass(node, testClass)
+      expect(node.classList[0]).to.equal(testClass)
+    }) 
 
-        expect(node.classList[0]).to.be.undefined
-      })    
+    it('Should remove a class from the node provided', () => {
+      const node = document.createElement('i')
+      const testClass = 'test'
+      node.classList.add(testClass)
+      toggleClass(node, testClass)
+
+      expect(node.classList[0]).to.be.undefined
+    })    
   })
 
   describe('hasClass()', () => {
-      it('Should return true if node has class', () => {
-        const node = document.createElement('i')
-        const testClass = 'test'
-        node.classList.add(testClass)      
+    it('Should return true if node has class', () => {
+      const node = document.createElement('i')
+      const testClass = 'test'
+      node.classList.add(testClass)      
 
-        expect(hasClass(node, testClass)).to.be.true
-      })  
-      it('Should return false if node doesn\'t have class', () => {
-        const node = document.createElement('i')
-        const testClass = 'test'
+      expect(hasClass(node, testClass)).to.be.true
+    })  
+    
+    it('Should return false if node doesn\'t have class', () => {
+      const node = document.createElement('i')
+      const testClass = 'test'
 
-        expect(hasClass(node, testClass)).to.be.false
-      })  
+      expect(hasClass(node, testClass)).to.be.false
+    })  
+  })
+
+  describe('removeClass()', () => {
+    it('Should remove class from node', () => {
+      const node = document.createElement('i')
+      const testClass = 'test'
+      node.classList.add(testClass)      
+      removeClass(node, testClass)
+      expect(node.classList[0]).to.be.undefined
+    })  
+  })
+
+  describe('addClass()', () => {
+    it('Should add a class to a node', () => {
+      const node = document.createElement('i')
+      const testClass = 'test'     
+      addClass(node, testClass)
+      expect(node.classList[0]).to.equal(testClass)
+    })  
   })
 })
